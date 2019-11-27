287c5dbbed249d7be67e43daa7601eaffc2f5dfe
Valantina80 <a.f.@hotmail.fr>
2019-11-27 07:22:03 +0100

    xml

--- Lambert_Anne-France/jour3/XmlParser.php
+++ Lambert_Anne-France/jour3/XmlParser.php
@@ -1,25 +1,47 @@
 <?php
 namespace LambertAnne_France;
 use XMLReader;
+use DOMDocument;
 // https://www.php.net/manual/en/class.xmlreader.php
 // "This is my new child of XML parsing method  based on my and yours modification."
 class XmlParser extends Parser
 {
     private $xml;
+    private $name;
 
-    public function __construct($xml)
+    public function __construct($name)
     {
-        $xml= new XMLReader($xml);
-        $this->xml=$xml;
-        parent::__construct($xml);
+        $this->xml= new XMLReader();
+
+       // $this->xml=$xml;
+        $this->name=$name;
+        parent::__construct($this->xml);
     }
     public function entete()
     {
-        // Open XML file
-        
+        $doc=new DOMDocument;
+        if (!$this->xml->open($this->name)) {
+            die("Impossible d'ouvrir '".$this->name."'");
+        }
 
-        // // Parse Xml and return result
-        return $this->xml->open($this->xml)->xmlparseXml($this->xml);
+        while($this->xml->read()) {
+            if ($this->xml->nodeType == XMLReader::ELEMENT && $this->xml->name == 'personne') {
+                $node = simplexml_import_dom($doc->importNode($this->xml->expand(), true));
+                foreach ($node as $key => $value) {
+                    keyTab=array[];
+                    //mettre clé dans array
+                    echo("hey".$key) ;
+
+                    //$entete=$this->entete()[$i];
+                    //$o->$entete=$ligne[$i];
+                }
+               // echo $node->nom;
+              //  echo $node->prenom;
+               // echo $node->age;
+            }
+        }
+        $this->xml->close();
+    
     }
     public function donnees()
     {
